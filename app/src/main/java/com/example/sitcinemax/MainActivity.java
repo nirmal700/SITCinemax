02e857add4d27fb5d9d634fbcd47bc12719b08f5
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-01-08 03:37:22 +0530

    Splash Screen and User Sign Up

--- app/src/main/java/com/example/sitcinemax/MainActivity.java
+++ app/src/main/java/com/example/sitcinemax/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.sitcinemax;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation bottom,side;
+
+    private static int SPLASH_TIMER = 3400;
+
+    TextView powered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        powered=findViewById(R.id.powered);
+
+        //Splash Animation
+        side= AnimationUtils.loadAnimation(this,R.anim.side_anim);
+        bottom= AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+
+        powered.setAnimation(bottom);
     }
 }
