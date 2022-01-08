032ba5ad6a90b43875e9c799ab8b7e0c19637609
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-01-08 17:52:44 +0530

    Update ContactUs.java

--- app/src/main/java/com/example/sitcinemax/ContactUs.java
+++ app/src/main/java/com/example/sitcinemax/ContactUs.java
@@ -68,14 +68,14 @@ public class ContactUs extends AppCompatActivity {
                 Intent i = new Intent(Intent.ACTION_VIEW);
 
                 try {
-                    String url = "https://api.whatsapp.com/send?phone="+ "+919847832419" +"&text=" + URLEncoder.encode("Hai,\n", "UTF-8");
+                    String url = "https://api.whatsapp.com/send?phone="+ "+917008000094" +"&text=" + URLEncoder.encode("Hai,\n", "UTF-8");
                     i.setPackage("com.whatsapp");
                     i.setData(Uri.parse(url));
                     startActivity(i);
                 } catch (Exception e){
                     e.printStackTrace();
                 }
-                Toast.makeText(ContactUs.this, "Contact Nabeeh P P via Whatsapp", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ContactUs.this, "Contact SIT Cinemax via Whatsapp", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -83,7 +83,7 @@ public class ContactUs extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Uri uri = Uri.parse("https://instagram.com/anas_mohmd__?utm_medium=copy_link");
+                Uri uri = Uri.parse("https://www.instagram.com/k.nirmalkumar2002/");
                 Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
                 likeIng.setPackage("com.instagram.android");
 
@@ -91,9 +91,9 @@ public class ContactUs extends AppCompatActivity {
                     startActivity(likeIng);
                 } catch (ActivityNotFoundException e) {
                     startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("https://instagram.com/anas_mohmd__?utm_medium=copy_link")));
+                            Uri.parse("https://www.instagram.com/k.nirmalkumar2002/")));
                 }
-                Toast.makeText(ContactUs.this, "Contact Mohamed Anas K P via Instagram", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ContactUs.this, "Contact SIT Cinemax via Instagram", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -103,10 +103,10 @@ public class ContactUs extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent emailIntent = new Intent(Intent.ACTION_VIEW);
-                Uri data = Uri.parse("mailto:?subject=" +"QR Registry"+ "&body=" + "Hai,\n" + "&to=" + "salmanulfariscc1999@gmail.com");
+                Uri data = Uri.parse("mailto:?subject=" +"SIT Cinemax"+ "&body=" + "Hai,\n" + "&to=" + "k.nirmalkumar2002@gmail.com");
                 emailIntent.setData(data);
                 startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-                Toast.makeText(ContactUs.this, "Contact Salmanul Faris C C via E-mail", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ContactUs.this, "Contact SIT Cinemax via E-mail", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -115,11 +115,11 @@ public class ContactUs extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String url = "https://github.com/arshidabdulazeez";
+                String url = "https://github.com/nirmal700";
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
                 startActivity(i);
-                Toast.makeText(ContactUs.this, "Contact Arshid Abdul Azeez M P via GitHub", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ContactUs.this, "Contact Nirmal via GitHub", Toast.LENGTH_SHORT).show();
 
             }
         });
