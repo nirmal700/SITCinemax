1cd30c3c419729c3c07677a9a0ba9a5496a725d3
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-01-12 17:15:34 +0530

    UserBookTicket Changes

--- app/src/main/java/com/example/sitcinemax/UserBookTickets.java
+++ app/src/main/java/com/example/sitcinemax/UserBookTickets.java
@@ -1,26 +1,22 @@
 package com.example.sitcinemax;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -28,14 +24,10 @@ import java.util.Objects;
 public class UserBookTickets extends AppCompatActivity implements UserMoviesAdapter.OnItemClickListener{
 
     private RecyclerView recyclerView;
-    private ProgressDialog progressDialog;
     ImageView btn_back;
     private ArrayList<Movies> list;
     private UserMoviesAdapter userMoviesAdapter;
 
-    SessionManager manager;
-
-    String phoneNumber;
     private final CollectionReference collectionReference = FirebaseFirestore.getInstance().collection("Movies");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +40,7 @@ public class UserBookTickets extends AppCompatActivity implements UserMoviesAdap
                     .addOnFailureListener(this, exception -> Log.e("FireBase Anonymous ", "signInAnonymously:FAILURE", exception));
         }
         //Initialize ProgressDialog
-        progressDialog = new ProgressDialog(UserBookTickets.this);
+        ProgressDialog progressDialog = new ProgressDialog(UserBookTickets.this);
         progressDialog.show();
         progressDialog.setContentView(R.layout.progress_dialog);
         progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
@@ -66,31 +58,27 @@ public class UserBookTickets extends AppCompatActivity implements UserMoviesAdap
         userMoviesAdapter.setOnItemClickListener(UserBookTickets.this);
         progressDialog.dismiss();
 
-        btn_back.setOnClickListener(v -> {
-            onBackPressed();
-        });
+        btn_back.setOnClickListener(v -> onBackPressed());
         LoadRecycler();
 
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private void LoadRecycler() {
         Query mMovie = collectionReference.whereEqualTo("IsScreening",true);
-        mMovie.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (error != null) {
-                    Log.e("AddSnapShot", error.getMessage());
-                    return;
-                }
-                for(DocumentChange documentChange : Objects.requireNonNull(value).getDocumentChanges())
-                {
-                    if (documentChange.getType() == DocumentChange.Type.ADDED) {
-                        list.add(documentChange.getDocument().toObject(Movies.class));
-                        userMoviesAdapter = new UserMoviesAdapter(UserBookTickets.this, list);
-                        recyclerView.setAdapter(userMoviesAdapter);
-                        userMoviesAdapter.notifyDataSetChanged();
-                        userMoviesAdapter.setOnItemClickListener(UserBookTickets.this);
-                    }
+        mMovie.addSnapshotListener((value, error) -> {
+            if (error != null) {
+                Log.e("AddSnapShot", error.getMessage());
+                return;
+            }
+            for(DocumentChange documentChange : Objects.requireNonNull(value).getDocumentChanges())
+            {
+                if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                    list.add(documentChange.getDocument().toObject(Movies.class));
+                    userMoviesAdapter = new UserMoviesAdapter(UserBookTickets.this, list);
+                    recyclerView.setAdapter(userMoviesAdapter);
+                    userMoviesAdapter.notifyDataSetChanged();
+                    userMoviesAdapter.setOnItemClickListener(UserBookTickets.this);
                 }
             }
         });
