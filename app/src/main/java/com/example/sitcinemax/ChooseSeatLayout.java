65775c9f6abdfdf5f94ba8e5a0643a88e8c32bc5
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-01-12 11:32:06 +0530

    Update ChooseSeatLayout.java

--- app/src/main/java/com/example/sitcinemax/ChooseSeatLayout.java
+++ app/src/main/java/com/example/sitcinemax/ChooseSeatLayout.java
@@ -13,14 +13,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
 import java.util.List;
