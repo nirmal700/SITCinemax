1496cf4ce0068c63c41d63d0fa535896865b6da2
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-01-15 04:50:24 +0530

    Update AboutMovie.java

--- app/src/main/java/com/example/sitcinemax/AboutMovie.java
+++ app/src/main/java/com/example/sitcinemax/AboutMovie.java
@@ -1,16 +1,11 @@
 package com.example.sitcinemax;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -58,12 +53,9 @@ public class AboutMovie extends YouTubeBaseActivity {
             }
         };
         youTubePlayerView.initialize(api_key,onInitializedListener);
-        btn_backToSd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(AboutMovie.this,UserBookTickets.class));
-                finishAffinity();
-            }
+        btn_backToSd.setOnClickListener(v -> {
+            startActivity(new Intent(AboutMovie.this,UserBookTickets.class));
+            finishAffinity();
         });
     }
 }
