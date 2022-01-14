848ff84897653c184a6c90c647340a808d53b9c9
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-01-15 05:07:25 +0530

    Code Optimization

--- app/src/main/java/com/example/sitcinemax/UserSignUp.java
+++ app/src/main/java/com/example/sitcinemax/UserSignUp.java
@@ -5,7 +5,6 @@ import android.app.ActivityOptions;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
--- app/src/main/java/com/example/sitcinemax/UserToDoList.java
+++ app/src/main/java/com/example/sitcinemax/UserToDoList.java
@@ -8,11 +8,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -82,12 +80,9 @@ public class UserToDoList extends AppCompatActivity {
             }
         });
 
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(UserToDoList.this, UserDashBoard.class));
-                finishAffinity();
-            }
+        btn_back.setOnClickListener(v -> {
+            startActivity(new Intent(UserToDoList.this, UserDashBoard.class));
+            finishAffinity();
         });
 
         //Initialize ProgressDialog
@@ -95,109 +90,91 @@ public class UserToDoList extends AppCompatActivity {
 
         list(); // Load all data
 
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btn_add.setOnClickListener(v -> {
 
-                Calendar calendar = Calendar.getInstance();
-                final int year = calendar.get(Calendar.YEAR);
-                final int month = calendar.get(Calendar.MONTH);
-                final int day = calendar.get(Calendar.DAY_OF_MONTH);
+            Calendar calendar = Calendar.getInstance();
+            final int year = calendar.get(Calendar.YEAR);
+            final int month = calendar.get(Calendar.MONTH);
+            final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-                final Dialog dialog = new Dialog(UserToDoList.this);
+            final Dialog dialog = new Dialog(UserToDoList.this);
 
-                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                dialog.setContentView(R.layout.todo_add_and_update);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.todo_add_and_update);
 
-                dialog.show();
-                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.getWindow().setWindowAnimations(R.style.BottomDialog);
-                dialog.getWindow().setGravity(Gravity.BOTTOM);
+            dialog.show();
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().setWindowAnimations(R.style.BottomDialog);
+            dialog.getWindow().setGravity(Gravity.BOTTOM);
 
-                Button btn_selectDate = dialog.findViewById(R.id.btn_selectDate);
-                EditText et_title = dialog.findViewById(R.id.et_title);
-                EditText et_desc = dialog.findViewById(R.id.et_description);
-                Button btn_new = dialog.findViewById(R.id.bt_ok);
+            Button btn_selectDate = dialog.findViewById(R.id.btn_selectDate);
+            EditText et_title = dialog.findViewById(R.id.et_title);
+            EditText et_desc = dialog.findViewById(R.id.et_description);
+            Button btn_new = dialog.findViewById(R.id.bt_ok);
 
-                Button btn_cancel = dialog.findViewById(R.id.btn_cancel);
+            Button btn_cancel = dialog.findViewById(R.id.btn_cancel);
 
-                btn_cancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
+            btn_cancel.setOnClickListener(v1 -> dialog.dismiss());
 
 
-                btn_selectDate.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        DatePickerDialog datePickerDialog = new DatePickerDialog(
-                                UserToDoList.this, new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int month, int day) {
-                                month = month + 1;
+            btn_selectDate.setOnClickListener(v12 -> {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        UserToDoList.this, (view, year1, month1, day1) -> {
+                            month1 = month1 + 1;
 
-                                String fd = "" + day;
-                                String fm = "" + month;
-                                if (day < 10) {
-                                    fd = "0" + day;
-                                }
-                                if (month < 10) {
-                                    fm = "0" + month;
-                                }
-
-                                selectedDate = fd + "/" + fm + "/" + year;
-
-                                btn_selectDate.setText(selectedDate);
+                            String fd = "" + day1;
+                            String fm = "" + month1;
+                            if (day1 < 10) {
+                                fd = "0" + day1;
                             }
+                            if (month1 < 10) {
+                                fm = "0" + month1;
+                            }
+
+                            selectedDate = fd + "/" + fm + "/" + year1;
+
+                            btn_selectDate.setText(selectedDate);
                         }, year, month, day);
-                        datePickerDialog.show();
+                datePickerDialog.show();
+            });
+
+
+            btn_new.setOnClickListener(v13 -> {
+
+                if (et_title.getText().toString().trim().isEmpty() | et_desc.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(UserToDoList.this, "Do not empty Title and Description", Toast.LENGTH_SHORT).show();
+                } else if (btn_selectDate.getText().toString().isEmpty()) {
+                    Toast.makeText(UserToDoList.this, "Please select a date", Toast.LENGTH_SHORT).show();
+                } else {
+
+
+                    String date = btn_selectDate.getText().toString();
+
+                    String nTitle = et_title.getText().toString();
+                    String nDesc = et_desc.getText().toString();
+
+                    String[] separated = date.split("/");
+
+                    String newDay = separated[0];
+                    String newMonth = separated[1];
+                    String newYear = separated[2];
+
+                    String id = todoDb.push().getKey();
+                    TodoModel model = new TodoModel(id, nTitle, nDesc, newDay, newMonth, newYear);
+
+                    if (id != null) {
+                        todoDb.child(id).setValue(model);
                     }
-                });
 
+                    dialog.dismiss();
+                    Toast.makeText(UserToDoList.this, "New item listed", Toast.LENGTH_SHORT).show();
+                    loadProgressDialog();
+                    list();
 
-                btn_new.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+                }
 
-                        if (et_title.getText().toString().trim().isEmpty() | et_desc.getText().toString().trim().isEmpty()) {
-                            Toast.makeText(UserToDoList.this, "Do not empty Title and Description", Toast.LENGTH_SHORT).show();
-                        } else if (btn_selectDate.getText().toString().isEmpty()) {
-                            Toast.makeText(UserToDoList.this, "Please select a date", Toast.LENGTH_SHORT).show();
-                        } else {
-
-
-                            String date = btn_selectDate.getText().toString();
-
-                            String nTitle = et_title.getText().toString();
-                            String nDesc = et_desc.getText().toString();
-
-                            String[] separated = date.split("/");
-
-                            String newDay = separated[0];
-                            String newMonth = separated[1];
-                            String newYear = separated[2];
-
-                            String id = todoDb.push().getKey();
-                            TodoModel model = new TodoModel(id, nTitle, nDesc, newDay, newMonth, newYear);
-
-                            if (id != null) {
-                                todoDb.child(id).setValue(model);
-                            }
-
-                            dialog.dismiss();
-                            Toast.makeText(UserToDoList.this, "New item listed", Toast.LENGTH_SHORT).show();
-                            loadProgressDialog();
-                            list();
-
-                        }
-
-                    }
-                });
-
-            }
+            });
 
         });
 
