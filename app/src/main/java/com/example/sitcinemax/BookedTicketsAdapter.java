39ed8ecae191fdc9313dfca379e6a35cec7b8166
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-01-15 04:45:46 +0530

    Update BookedTicketsAdapter.java

--- app/src/main/java/com/example/sitcinemax/BookedTicketsAdapter.java
+++ app/src/main/java/com/example/sitcinemax/BookedTicketsAdapter.java
@@ -2,24 +2,17 @@ package com.example.sitcinemax;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BookedTicketsAdapter extends RecyclerView.Adapter<BookedTicketsAdapter.MyViewHolder> {
